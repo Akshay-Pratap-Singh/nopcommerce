a7a8c0f33aae2c88f3ef1cfe993dde04b1cbe307
Akshay <akshaysingh2403@gmail.com>
2022-06-21 01:11:05 +0530

    again commit

--- run.bat
+++
@@ -1,4 +0,0 @@
-pytest -v -m "sanity" --html=Reports/reports.html testCases/ --browser chrome
-rem pytest -v -m "regression" --html=Reports/reports.html testCases/ --browser chrome
-rem pytest -v -m "sanity or regression" --html=Reports/reports.html testCases/ --browser chrome
-rem pytest -v -m "sanity and regression" --html=Reports/reports.html testCases/ --browser chrome
